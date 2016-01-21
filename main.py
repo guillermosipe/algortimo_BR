3738c09a8ec9be09455b78d92be503152226cd36
Guillermo Silva Peña <guillermo.sipe@gmail.com>
2016-01-20 23:53:54 -0600

    Corrección función tipico y agregar nuevos elementos a la cola

--- main.py
+++ main.py
@@ -19,11 +19,12 @@ def testor(matrix):
 	return True
 
 def tipico(matrix):
-	mask = matrix[0]
-	for x in range(len(matrix)-1):
-		mask = [x + y for x, y in zip(mask, matrix[x+1])]
-	#print (mask)
-	if collections.Counter(mask)[0] != 0:
+	one_position = []
+	for line in matrix:
+		line_one = [x for x,y in enumerate(line) if y==1]
+		if len(line_one)==1 and line_one[0] not in one_position:
+				one_position.append(line_one[0])
+	if(len(one_position)!=len(matrix[0])):
 		return False
 	return True
 
@@ -89,25 +90,37 @@ def br_algorithm(matrix):
 	queue =  [[x] for x in matrix_order[0]]
 	matrix_order = matrix_order[0] + matrix_order[1]
 	psi_star = []
+	column_num = len(matrix[0])
 
 	while(queue):
+	#for x in range(0, 3):
 		elements = queue.pop(0)
 		aux_matrix = sub_matrix(matrix,elements)
+		queue_aux = queue[:]
+		action = ""
 		#print_matrix(aux_matrix)
 		if(testor(aux_matrix) and tipico(aux_matrix)):
 			psi_star.append(elements)
 			queue = superset_delete(queue,elements)
+			action = "testor y tipico"
 		elif(excluyente(aux_matrix)):
+			action = "excluyente"
 			queue = superset_delete(queue,elements)
 		else:
+			action = "fuera"
 			new_elements = []
-			for x in range(len(matrix[0])):
+			#for x in range(max(elements),column_num):
+			for x in range(matrix_order.index(max(elements)),column_num):
 				if(x not in elements):
 					new_elements.append(elements+[x])
 			queue = push_queue(queue,new_elements)
-			#print(queue)
-	print("Testores Típics:")	
-	print_matrix(psi_star)
+		#print(elements,"\n",action,"\n",queue_aux,"\n",queue,"\n\n")
+	print("Testores Típicos:")	
+	for testor_print in psi_star:
+		print(sorted(testor_print),"\n")
+		print_matrix(sub_matrix(matrix,testor_print))
+	print("Número: ",len(psi_star))
+	#print_matrix(psi_star)
 
 
 # -------------------------------------------------------------------------------------
@@ -129,7 +142,26 @@ if __name__ == '__main__':
 				[1,0,0,0],
 				[0,0,1,0],
 				[0,1,0,0]]
-
-	br_algorithm(matrix_2)
 	
-
+	br_algorithm(matrix_1)
+	
+""" 
+    Testores tipícos: 14
+    	[5,7,8] 
+    	[4,7,5] 
+    	[1,7,8] 
+    	[1,7,4] 
+    	[0,1,2,6] 
+    	[0,1,2,3] 
+    	[0,1,6,7] 
+    	[0,1,3,7] 
+    	[0,5,8] 
+    	[0,4,8] 
+    	[0,2,5] 
+    	[0,1,8] 
+    	[0,1,4] 
+    	[0,5,7] 
+	Contador de hits: sacar de la pila -> 92
+	tamaño maximo de la lista pila
+	cantidad de memoria 
+"""
