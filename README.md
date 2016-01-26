def0b50d4a9b4f231b296102eb98929dd5354b7f
Guillermo Silva Peña <guillermo.sipe@gmail.com>
2016-01-26 17:10:57 -0600

    Optimización función excluyente y agregar función para leer matriz de archivo externo (json)

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Publicación: [BR: A New Method for Computing All Typical Testors](http://link.s
 ```shell
 python main.py
 ```
-
+### Imprimir pasos
 Se puede imprimir los pasos que sigue el algoritmo para obtener los testores típicos mediante la siguiente opción:
 
 ```shell
@@ -15,7 +15,7 @@ python main.py -i Y
 python main.py -imprimir_pasos Y
 ```
 
-
+### Seleccionar matriz de prueba
 Se cuenta con las siguientes matrices de prueba:
 
 Matriz "articulo_br" [BR: A New Method for Computing All Typical Testors](http://link.springer.com/chapter/10.1007%2F978-3-642-10268-4_50#page-1).
@@ -51,4 +51,21 @@ Para hacer uso de alguna matriz en especifico:
 ```shell
 python main.py -m ['articulo_br','identidad_basica','n2', 's']
 python main.py -matriz ['articulo_br','identidad_basica','n2', 's']
+```
+
+### Cargar matriz externa
+Se puede cargar una matriz externa a través de un archivo JSON con la siguiente estructura:
+
+```json
+{
+  "matrix":[[1,1,0,0,0,0],
+            [0,0,1,0,1,1],
+            [0,1,0,1,1,1]]
+}
+```
+
+La ejecución sería la siguiente:
+```shell
+python main.py -f matriz.json
+python main.py --file matriz.json
 ```
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+#Algoritmo BR
+#Desarrollador por: Silva Peña Guillermo
+#Descripcion: Implementación en Python del algoritmo BR
+
 # -------------------------------------------------------------------------------------
 # ---------------------------- Libraries ----------------------------------------------
 # -------------------------------------------------------------------------------------
@@ -7,6 +11,7 @@
 import os
 import collections
 from optparse import OptionParser
+import json
 
 # -------------------------------------------------------------------------------------
 # ---------------------------- Functions ----------------------------------------------
@@ -30,29 +35,16 @@ def tipico(matrix):
 	return True
 
 def order_matrix(matrix):
-	new_matrix = []
-	#for line in matrix:
 	one_position = []
 	zero_position = []
 	for i in range(len(matrix[0])):
-		#print (i,matrix[0][i])
 		if matrix[0][i]==1:
 			one_position.append(i)
 		else:
 			zero_position.append(i)
 	return one_position ,zero_position
 
-def mascara(matrix):
-	mascara = []
-	for x in range(len(matrix)):
-		elements_in_line=collections.Counter(matrix[x][0:-1])
-		if(elements_in_line[1]==1):
-			mascara.append(x)
-	return mascara
-
 def excluyente(matrix):
-	fastidia_columnas = 0
-	num_columnas_uno = 0
 	mismo_cero = True
 	fastidia = False
 	mascara = {}
@@ -61,32 +53,21 @@ def excluyente(matrix):
 		return False
 	for x in range(len(matrix)):
 		elements_in_line=collections.Counter(matrix[x][0:-1])
-		test = list(enumerate(matrix[x][0:-1]))
-		test = list(filter(lambda x: x[1] == 1, test))
-		#print("-",test)
-		if(len(test)==1):
-			for t in test:
-				if mascara.get(t[0], False):
-					mascara[t[0]].append(x)
-				else:
-					mascara[t[0]] = [x]
-		#print(matrix[x][-1])
+		if elements_in_line[1]== 1:
+			pos_one = matrix[x][0:-1].index(1)
+			if mascara.get(pos_one, False):
+				mascara[pos_one].append(x)
+			else:
+				mascara[pos_one] = [x]
 		if matrix[x][-1]==1:
 			mascara_add.append(x)
 		# Mismo numero de renglones en 0
 		if elements_in_line[0] == len(matrix[x][0:-1]) and matrix[x][-1] == 1:
 			mismo_cero = False
-
 	# Fastidia el unico renglon tipico de alguna columna anterior
-	#print(mascara,mascara_add)
 	for m in mascara:
-		#print(m,mascara[m])
 		if set(mascara[m]) <= set(mascara_add):
-			#print("SI")
 			fastidia = True
-
-	#if(num_columnas_uno==fastidia_columnas):
-	#	fastidia = True
 	return fastidia or mismo_cero
 
 def print_matrix(matrix):
@@ -126,9 +107,9 @@ def br_algorithm(matrix,print_steps=False):
 	psi_star = []
 	column_num = len(matrix[0])
 	hits = 0
-
+	steps = 0
 	while(queue):
-	#for x in range(0, 12):
+		steps += 1
 		hits += 1
 		queue_aux = queue[:]
 		elements = queue.pop(0)
@@ -136,16 +117,12 @@ def br_algorithm(matrix,print_steps=False):
 		if(testor(aux_matrix) and tipico(aux_matrix)):
 			action = "Es Testor y típico"
 			psi_star.append(elements)
-			aux_hits = len(queue)
 			queue = superset_delete(queue,elements)
-			aux_hits -= len(queue)
-			hits += aux_hits
+			hits += len(queue_aux) - len(queue) -1
 		elif(excluyente(aux_matrix)):
 			action = "Es excluyente"
-			aux_hits = len(queue)
 			queue = superset_delete(queue,elements)
-			aux_hits -= len(queue)
-			hits += aux_hits
+			hits += len(queue_aux) - len(queue) -1
 		else:
 			action = "Es candidato"
 			new_elements = []
@@ -153,16 +130,17 @@ def br_algorithm(matrix,print_steps=False):
 				new_elements.append(elements+[matrix_order[x]])
 			queue = push_queue(queue,new_elements)
 		if(print_steps==True):
-			print("Paso:",hits,"\nPila inicial:",queue_aux,"\nElemento:",elements)
+			print("Paso:",steps,"\nPila inicial:",queue_aux,"\nElemento:",elements)
 			print_matrix(sub_matrix(matrix,elements))
 			print("Acción:",action,"\nPila final",queue,"\n\n")
 	print("\nTestores Típicos:")
 	for testor_print in psi_star:
 		print(sorted(testor_print))
-		#print_matrix(sub_matrix(matrix,testor_print))
+		if(print_steps==True):
+			print_matrix(sub_matrix(matrix,testor_print))
 	print("Número:",len(psi_star))
 	print("Hits:",hits)
-	#print_matrix(psi_star)
+	return
 
 
 # -------------------------------------------------------------------------------------
@@ -216,45 +194,26 @@ if __name__ == '__main__':
 	parser.add_option("-m", "--matriz",
                       type='choice',
                       action='store',
-                      dest='matrix',
+                      dest='matriz',
                       choices=['articulo_br','identidad_basica','n2', 's'],
                       default='articulo_br',
                       help="Selecciona la matriz a analizar: articulo_br,identidad_basica,n2,s")
 
+	parser.add_option("-f", "--file",
+                      dest='archivo_matriz',
+					  default = False,
+                      help="Inserta una matriz desde un archivo")
+
 	(options, args) = parser.parse_args()
 
-	#excluyente
-	"""sub_matrix_ = sub_matrix(matrix_1,[0,7,4])
-	print_matrix(sub_matrix_)
-	print(excluyente(sub_matrix_))"""
-
-	"""sub_matrix_ = sub_matrix(matrix_1,[0,2,4])
-	print_matrix(sub_matrix_)
-
-	print(excluyente(sub_matrix_))"""
+	if(options.archivo_matriz):
+		with open(options.archivo_matriz,'r') as json_file:
+			archivo_matriz = json.load(json_file)
+		matriz_seleccionada = archivo_matriz["matrix"]
+	else:
+		matriz_seleccionada = matrix[options.matriz]
 
 	if options.imprimir_pasos=='Y':
-		br_algorithm(matrix[options.matrix],True)
+		br_algorithm(matriz_seleccionada,True)
 	else:
-		br_algorithm(matrix[options.matrix])
-
-"""
-    Testores tipícos: 14
-    	[5,7,8]
-    	[4,7,5]
-    	[1,7,8]
-    	[1,7,4]
-    	[0,1,2,6]
-    	[0,1,2,3]
-    	[0,1,6,7]
-    	[0,1,3,7]
-    	[0,5,8]
-    	[0,4,8]
-    	[0,2,5]
-    	[0,1,8]
-    	[0,1,4]
-    	[0,5,7]
-	Contador de hits: sacar de la pila -> 92
-	tamaño maximo de la lista pila
-	cantidad de memoria
-"""
+		br_algorithm(matriz_seleccionada)
